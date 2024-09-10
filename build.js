c7ba5f6a52c8ac11d0cccea9a7bdc01a1cf5bc7a
samlinz <lindqvist.samuel@gmail.com>
2024-09-11 00:16:24 +0300

    Update build.js

--- build.js
+++ build.js
@@ -2,9 +2,8 @@ const esbuild = require("esbuild");
 
 esbuild
   .build({
-    entryPoints: ["src/test.ts", "src/background.ts"],
+    entryPoints: ["src/background.ts"],
     bundle: true,
-    // outfile: "out/test.js",
     outdir: "out",
     minify: true,
     sourcemap: true,
